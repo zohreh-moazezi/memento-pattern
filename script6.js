2858a90a10385c6f39e4d24688bb62d8b1fcf581
zohreh moazezi <zohre.m548@gmail.com>
2023-02-28 15:37:38 +0330

    memento

---
+++ script6.js
@@ -0,0 +1,15 @@
+// Memento Pattern
+
+// providing temporary state of an object
+// and restoration of that object from a conversion into a different format...
+
+// often used into serialization and deserialization of data
+// JSON <=> JS Object
+
+// You could view a database as an implementation of the Memento design pattern
+//     in which objects are persisted and restored. However, the most common reason
+//     for using this pattern is to capture a snapshot of an object’s state so that
+//     any subsequent changes can be undone easily if necessary.
+
+// we don't need it now...
+// https://www.dofactory.com/javascript/design-patterns/memento
